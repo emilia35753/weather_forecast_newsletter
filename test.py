84f0b553223c113bfb0516f40f48b15fff4cc7c5
emilia35753 <emilia.lupa4321@gmail.com>
2026-03-25 20:11:53 +0100

    Updated test.py to actually send today's forecast

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from weather_data import get_weather_forecast, extract_forecast_data, group_by_day
+from weather_data import get_weather_forecast, extract_forecast_data, group_by_day, get_today_forecast
 from analysis import calculate_daily_stats
 from visualization import create_temperature_chart, create_statistics_summary
 from email_sender import send_email
@@ -11,12 +11,19 @@ def test_01():
     #for date, temp, descr in temperatures_with_dates:
     #    print(f"Data:{date}, temp:{temp}, description:{descr}\n")
 
+    # getting today's detailed forecast
+    today_forecast = get_today_forecast(temperatures_with_dates)
+
+    # getting 5-day forecast
     daily_data = group_by_day(temperatures_with_dates)
     #print(daily_data)
     daily_stats = calculate_daily_stats(daily_data)
+
+    # creating charts
     create_temperature_chart(daily_stats)
     create_statistics_summary(daily_stats)
 
+    # sending emails
     chart_files = ['temp_forecast.png', 'statistics_summary.png']
 
     send_email(
@@ -24,5 +31,7 @@ def test_01():
         sender_password=config.SENDER_PASSWORD,
         recipient_email=config.RECIPIENT_EMAIL,
         daily_stats=daily_stats,
-        chart_files=chart_files
+        chart_files=chart_files,
+        today_forecast=today_forecast
+
     )
