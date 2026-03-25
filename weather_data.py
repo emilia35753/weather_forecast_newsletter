a452b04fcd67ff8f2c9fdb281dd64bd81014bcc5
emilia35753 <emilia.lupa4321@gmail.com>
2026-03-25 19:50:17 +0100

    Added data extraction for today's more detailed forecast

--- weather_data.py
+++ weather_data.py
@@ -15,10 +15,35 @@ def get_weather_forecast(api_key, city):
     print(data['list'][0])
     return data
 
+
+def get_today_forecast(forecast_data):
+    if not forecast_data:
+        return []
+
+    # Get today's date from the first forecast
+    # forecast_data[0] is a tuple, forecast_data[0][0] is datetime string
+    today_date = forecast_data[0][0].split(' ')[0]
+
+    today_forecasts = []
+    for forecast in forecast_data:
+        datetime_str = forecast[0]  # first element of tuple
+        date = datetime_str.split(' ')[0]
+
+        if date == today_date:
+            time = datetime_str.split(' ')[1]  # getting just the time
+            today_forecasts.append({
+                'time': time,
+                'temp': forecast[1],
+                'feels_like': forecast[2],
+                'description': forecast[3]
+            })
+
+    return today_forecasts
+
 def extract_forecast_data(data):
     temperatures_with_dates = []
     for forecast in data['list']:
-        temperatures_with_dates.append((forecast['dt_txt'], forecast['main']['temp'], forecast['weather'][0]['description']))
+        temperatures_with_dates.append((forecast['dt_txt'], forecast['main']['temp'], forecast['main']['feels_like'], forecast['weather'][0]['description']))
 
     #print(temperatures_with_dates)
     return temperatures_with_dates
