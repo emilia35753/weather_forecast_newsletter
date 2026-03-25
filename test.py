fb01571f10db2576265e548bcc97c42ed2e2b9ba
emilia35753 <emilia.lupa4321@gmail.com>
2026-03-25 19:29:21 +0100

    Updated test.py to send one weather forecast email

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from weather_data import get_weather_forecast, extract_forecast_data, group_by_day
 from analysis import calculate_daily_stats
 from visualization import create_temperature_chart, create_statistics_summary
+from email_sender import send_email
+import config
 
 def test_01():
-    api_key = "5aad598ce580ee84bb7f06ca01883d5e"
-    city = "Kraków"
-    data = get_weather_forecast(api_key, city)
+    data = get_weather_forecast(config.API_KEY, config.CITY)
     temperatures_with_dates = extract_forecast_data(data)
 
     #for date, temp, descr in temperatures_with_dates:
@@ -16,3 +16,13 @@ def test_01():
     daily_stats = calculate_daily_stats(daily_data)
     create_temperature_chart(daily_stats)
     create_statistics_summary(daily_stats)
+
+    chart_files = ['temp_forecast.png', 'statistics_summary.png']
+
+    send_email(
+        sender_email=config.SENDER_EMAIL,
+        sender_password=config.SENDER_PASSWORD,
+        recipient_email=config.RECIPIENT_EMAIL,
+        daily_stats=daily_stats,
+        chart_files=chart_files
+    )
